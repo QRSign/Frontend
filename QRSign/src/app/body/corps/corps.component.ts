69395c8f964b76b75e594722e06d0cd4c08485a9
Logan <chenavier.logan@gmail.com>
2020-11-23 10:59:49 +0100

    tonio

--- QRSign/src/app/body/corps/corps.component.ts
+++ QRSign/src/app/body/corps/corps.component.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./corps.component.scss']
 })
 export class CorpsComponent implements OnInit {
-
+test:boolean=false
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  qrcode(){
+    this.test=!this.test
+  }
 }
