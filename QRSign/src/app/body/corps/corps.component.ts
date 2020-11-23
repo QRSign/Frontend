8f54a0b28c83fad8d55683111caf1bd5d4aff6e3
MTLantoine <mortelier.antoine@hotmail.fr>
2020-11-23 11:00:54 +0100

    getDate in progress

--- QRSign/src/app/body/corps/corps.component.ts
+++ QRSign/src/app/body/corps/corps.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as utils from '../../utils/utils';
+
 @Component({
   selector: 'app-corps',
   templateUrl: './corps.component.html',
-  styleUrls: ['./corps.component.scss']
+  styleUrls: ['./corps.component.scss'],
 })
 export class CorpsComponent implements OnInit {
+  todayDate: String;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    var now = new Date().toString().split(' ');
+    console.log('now', now);
   }
 
+  getTime() {
+    this.todayDate = utils.getTodayDate();
+  }
 }
