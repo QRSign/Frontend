4bf938b6cbb91e48853983a1c6e18c6ffdca4da2
Logan <logan@xps.home>
2020-12-08 12:14:35 +0100

    🚧 formulaire creation qr code

--- QRSign/src/app/qrcode-creation/qrcode-creation.component.ts
+++ QRSign/src/app/qrcode-creation/qrcode-creation.component.ts
@@ -3,11 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-qrcode-creation',
   templateUrl: './qrcode-creation.component.html',
-  styleUrls: ['./qrcode-creation.component.scss']
+  styleUrls: ['./qrcode-creation.component.scss'],
+  template: `<input type="datetime-local" [value]="date" 
+          (change)="date=$event.target.value" /> {{date}}` 
 })
 export class QrcodeCreationComponent implements OnInit {
-
-  constructor() { }
+  date: string;
+  constructor() {
+    this.date = new Date().toISOString().slice(0, 16);
+   }
 
   ngOnInit(): void {
   }
