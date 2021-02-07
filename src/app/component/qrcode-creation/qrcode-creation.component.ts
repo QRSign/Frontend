d713e5026b61ab502d7421eb08fa2411a7cc954e
MTLantoine <mortelier.antoine@hotmail.fr>
2021-02-07 02:18:16 +0100

    🔥 rm margin style

--- src/app/component/qrcode-creation/qrcode-creation.component.ts
+++ src/app/component/qrcode-creation/qrcode-creation.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
 
 import * as utils from '../../utils/utils';
 
@@ -11,9 +18,19 @@ export class QrcodeCreationComponent implements OnInit {
   date: string;
   title: string = 'Créer un nouveau cours';
 
-  constructor() {}
+  newCourseForm: FormGroup;
+  course_name = new FormControl('', [Validators.required]);
+
+  constructor(private fb: FormBuilder, private router: Router) {}
 
   ngOnInit(): void {
     this.date = utils.getValidDateFormat();
+    this.newCourseForm = this.fb.group({
+      course_name: this.course_name,
+    });
+  }
+
+  onSubmit(): void {
+    console.log('submited');
   }
 }
