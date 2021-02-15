222193907f99f3203ea39ef3eb64905ac5e36db2
MTLantoine <mortelier.antoine@hotmail.fr>
2021-02-16 00:41:59 +0100

    🎨 students presence request works

--- src/app/component/show-qrcode/show-qrcode.component.ts
+++ src/app/component/show-qrcode/show-qrcode.component.ts
@@ -8,11 +8,6 @@ export interface PeriodicElement {
   last_name: string;
   first_name: string;
 }
-const ELEMENT_DATA: PeriodicElement[] = [
-  { last_name: 'Dutoit', first_name: 'Antoine' },
-  { last_name: 'Fontaine', first_name: 'Nicolas' },
-  { last_name: 'Pascqualini', first_name: 'Lamya' },
-];
 
 @Component({
   selector: 'app-show-qrcode',
@@ -20,11 +15,11 @@ const ELEMENT_DATA: PeriodicElement[] = [
   styleUrls: ['./show-qrcode.component.scss'],
 })
 export class ShowQrcodeComponent implements OnInit {
-  subscriptions: Subscription[] = [];
+  token;
+  students;
 
   unfoundRoute: boolean = false;
   displayedColumns: string[] = ['Noms', 'Prénoms'];
-  dataSource = ELEMENT_DATA;
   public qrLink: string =
     'https://material.io/resources/icons/?search=class&style=baseline';
 
@@ -35,59 +30,41 @@ export class ShowQrcodeComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    // this.checkValidRoute();
+    this.checkValidRoute();
     this.onChange();
   }
 
   onChange(): void {
-    this.subscriptions.push(
-      this.route.paramMap.subscribe((param) => {
-        const token = param.get('token');
-        this.getStudents(token).subscribe(
-          (res) => {
-            console.log(res);
-          },
-          (err) => {
-            console.error(err);
-          }
-        );
-      })
-    );
-  }
-  getStudents(token: string): Observable<any> {
-    // setInterval(this.onChange, 3000);
-    // return this.requestService.request('get', ['signature', 'qrcode', token]);
-    // var jsonTest = {
-    //   first_name: 'Antereoine',
-    //   id: 5,
-    //   last_name: 'MOrteleeier',
-    //   signature: 'iVBORw0KGgoAAAANSUhEUgAAAcIAAAEsCAYAAABQVrO3AAAgAE',
-    //   token: 'f1e8a0fd',
-    // };
-    // jsonTest = JSON.stringify(jsonTest);
-    // return jsonTest;
-    return null;
+    const vm = this;
+    setInterval(() => vm.getStudents(), 1000);
   }
 
-  ngOnDestroy(): void {
-    this.subscriptions.forEach((sub) => sub.unsubscribe());
+  getStudents(): void {
+    this.hasStudents(this.token).subscribe(
+      (res) => {
+        this.students = res;
+      },
+      (err) => console.log(err)
+    );
+  }
+
+  hasStudents(token: string): Observable<any> {
+    return this.requestService.request('get', ['signature', 'qrcode', token]);
   }
 
   checkValidRoute(): void {
-    this.subscriptions.push(
-      this.route.paramMap.subscribe((param) => {
-        const token = param.get('token');
-        this.getToken(token).subscribe(
-          (res) => {
-            // this.router.navigateByUrl('/');
-          },
-          (err) => {
-            this.unfoundRoute = true;
-            console.error(err);
-          }
-        );
-      })
-    );
+    this.route.paramMap.subscribe((param) => {
+      const token = param.get('token');
+      this.getToken(token).subscribe(
+        (res) => {
+          this.token = token;
+        },
+        (err) => {
+          this.unfoundRoute = true;
+          console.error(err);
+        }
+      );
+    });
   }
 
   getToken(token: string): Observable<any> {
